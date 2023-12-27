7ca040d6c4cb7580b806228a13773098935ed6d7
bananaqick <bananaqick@gmail.com>
2023-12-27 12:01:47 +0900

    feat : 마이페이지 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Index } from '../components/layout/Index';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
+import { MyPage } from '../pages/MyPage';
 
 export const Router = () => {
   return (
@@ -9,6 +10,7 @@ export const Router = () => {
       <Route index element={<Index />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 };
