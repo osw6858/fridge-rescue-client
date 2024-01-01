6af46c14480c2923f8f96228f58439f33e1cbedd
DDIDDI <bananaqick@gmail.com>
2024-01-01 17:17:26 +0900

    feat : 요기 후기 등록 페이지 생성

---
+++ src/pages/ReviewPost.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { BasicInput } from '../components/common/BasicInput';
+import { BasicTitle } from '../components/common/BasicTitle';
+import { DragAndDrop } from '../components/common/DragAndDrop';
+import { BasicTextArea } from '../components/common/BasicTextArea';
+import { BasicButton } from '../components/common/BasicButton';
+import { theme } from '../styles/theme';
+import { useState } from 'react';
+
+export const ReviewPost = () => {
+  const [droppedImage, setDroppedImage] = useState<FormData | null>();
+
+  const handleImageDrop = (imageFile: FormData | null) => {
+    setDroppedImage(imageFile);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      title: { value: string };
+      content: { value: string };
+    };
+    const title = target.title.value;
+    const content = target.content.value;
+  };
+
+  return (
+    <ReviewPostContainer>
+      <BasicTitle title="요리 후기 등록" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <DragAndDrop text="완성된 요리의" onImageDrop={handleImageDrop} formDataKey="image" />
+        <div className="description">
+          <BasicInput type="text" placeholder="제목을 입력하세요" id="title" />
+          <BasicTextArea placeholder="요리 후기를 입력하세요" id="content" />
+        </div>
+        <div className="buttons">
+          <BasicButton
+            type="button"
+            $bgcolor={theme.colors.white}
+            $hoverbgcolor="#ececec"
+            $bordercolor="#c0c0c0"
+          >
+            돌아가기
+          </BasicButton>
+          <BasicButton
+            type="submit"
+            $bgcolor={theme.colors.orange}
+            $fontcolor={theme.colors.white}
+            $hoverbgcolor="#ff750c"
+          >
+            등록하기
+          </BasicButton>
+        </div>
+      </form>
+    </ReviewPostContainer>
+  );
+};
+
+const ReviewPostContainer = styled.div`
+  .description {
+    margin-top: 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    textarea {
+      height: 200px;
+    }
+  }
+
+  textarea {
+    height: 200px;
+  }
+
+  .buttons {
+    display: flex;
+    gap: 12px;
+    margin-top: 12px;
+
+    & > button {
+    }
+  }
+`;
