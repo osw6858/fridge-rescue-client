0579ce4640998491e41ac56e6c743f3975fd6de8
osw6858 <osw7875@naver.com>
2024-01-09 16:23:12 +0900

    fix: cors 문제 수정

--- src/api/recipe/getNewRecipe.ts
+++ src/api/recipe/getNewRecipe.ts
@@ -1,10 +1,11 @@
 import { END_POINTS } from '../../constants/api';
-import type { Recipe } from '../../types/recipeType';
 import { axiosDefault } from '../axiosInstance';
 
 export const getNewRecipe = async () => {
   try {
-    const response = await axiosDefault.get<Recipe[]>(END_POINTS.RECIPES);
+    const response = await axiosDefault.get(END_POINTS.RECIPES, {
+      params: { keyword: '당근', sortType: 'hot' },
+    });
     return response.data;
   } catch (error) {
     throw new Error(`${error}`);
