f577f5b10c98af498677eee4059fd647d3490ce0
bananaqick <bananaqick@gmail.com>
2024-01-11 11:51:12 +0900

    style : black 색상 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,7 @@ export const theme = {
     navy: '#003046',
     blue: '#009eba',
     sky: '#85cae4',
-    black: '#000000',
+    black: '#242424',
     darkGray: '#636773',
     gray: '#b3b4ba',
     lightGray: '#e1e2e3',
