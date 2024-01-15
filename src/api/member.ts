472ab1ffa33fdad911b88c5ba1878e100901d8e0
bananaqick <bananaqick@gmail.com>
2024-01-15 13:30:39 +0900

    feat : 비밀번호 변경 구현

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,6 +1,17 @@
+import type { PasswordFormData } from '../types/memberType';
 import { axiosAuth } from './axiosInstance';
 
 export const editNickname = async (nickname: string) => {
   const result = await axiosAuth.patch(`/members/info/nickname`, nickname);
+  return result.data;
+};
+
+export const editPassword = async (passwordInfo: PasswordFormData) => {
+  const result = await axiosAuth.patch(`/members/info/password`, {
+    currentPassword: passwordInfo.currentPassword,
+    newPassword: passwordInfo.editPassword,
+    newPasswordCheck: passwordInfo.editPassword,
+    // newPasswordCheck는 서버 재배포 시 삭제
+  });
   return result.data;
 };
---
+++ src/types/memberType.ts
@@ -0,0 +1,5 @@
+export interface PasswordFormData {
+  currentPassword: string;
+  editPassword: string;
+  editPasswordConfirm: string;
+}
