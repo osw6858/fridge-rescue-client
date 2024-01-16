4d63352f12dd1bc5376c7a3554400766f6d8e6df
osw6858 <osw7875@naver.com>
2024-01-16 12:40:12 +0900

    fix: 실패해도 재요청 가능하게 수정

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -76,7 +76,7 @@ export const handleTokenError = async (error: AxiosError) => {
         alert('토큰이 만료되었습니다. 다시 로그인해 주세요.');
         window.location.href = `${ROOT_URL}signin`;
       }
-
+      isRefreshing = false;
       return Promise.reject(error);
     }
   }
