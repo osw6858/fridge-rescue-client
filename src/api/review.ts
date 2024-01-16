7c314224ca247d820fa7e1295b344399de57a53a
bananaqick <bananaqick@gmail.com>
2024-01-16 17:50:48 +0900

    refactor : 엔드포인트 상수로 수정

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,6 +1,7 @@
+import { END_POINTS } from '../constants/api';
 import { axiosDefault } from './axiosInstance';
 
 export const getReviews = async (recipeId: string, page: number) => {
-  const result = await axiosDefault.get(`/recipes/${recipeId}/reviews?page=${page}`);
+  const result = await axiosDefault.get(`/${END_POINTS.RECIPES}/${recipeId}/reviews?page=${page}`);
   return result.data;
 };
